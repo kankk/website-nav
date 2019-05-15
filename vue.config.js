8ad7ee1b1c413b2f1177fdd0f0ec198b8aeea15f
KanKK <286454796@qq.com>
2019-05-15 14:48:44 +0800

    修改webpack配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
-module.exports = {
-  configureWebpack: {
+const isProd = process.env.NODE_ENV === 'production'
 
-  }
+// 开发环境配置
+const devConfig = {
+  devtool: 'source-map'
+}
+
+// 生产环境配置
+const prodConfig = {
+  devtool: ''
+}
+
+module.exports = {
+  configureWebpack: config => isProd ? prodConfig : devConfig
 }
